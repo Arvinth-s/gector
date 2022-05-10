edb275d0435eb615932f58b7e59df164675ede19
Arvinth-s <arvinth.saravana@gmail.com>
2022-05-10 14:46:45 +0000

    added various corruption rules

--- error_creator.py
+++ error_creator.py
@@ -4,7 +4,19 @@ from datasets import load_dataset
 import os
 from tqdm import tqdm
 import argparse
- 
+
+
+fixed_random_words='''தீவினுக்கோர் பாலம் அமைப்போம் -மகாகவி Welcome to delegates of Bharathi International நீல வண்ணத்தில் எழுத்துக்கள் வெள்ளைத் ...
+தென்னல் காற்று வீசுவது நின்று சுமார் ஒரு மாதகாலமாயிற்று. முன்னாள் ஜனாதிபதி மஹிந்த ராஜபக்ஷவினால் முன்னெடுக்கப்பட்ட போராட்டம் உட்பட வேலைநிறுத்த போராட்டங்களுக்கான நிதி அனுசரணையை சீனாவே வழங்கி நாட்டையும் அரசாங்கத்தையும் நெருக்கடிக்குள்ளாக்க முயல்கிறது என சமூக நலன்புரி பிரதி அமைச்சர் ரஞ்சன் ராமநாயக்க தெரிவித்தார்.
+நேற்று முன்னாள் ஜனாதிபதி மஹிந்த ராஜபக்ஷவின் தலைமையில் போராட்டம் முன்னெடுக்கப்பட்ட போராட்டத்தின் பின்னணியில் சீனாவே உள்ளது
+போராட்டங்கள் நடத்தும் அளவுக்கு மஹிந்த அணியினருக்கு எங்கிருந்து பணம் வருகின்றது
+ஆகவே மஹிந்த அணியினர் பேரணிக்கும் நாட்டில் நடத்தப்படும் வேலைநிறுத்த போராட்டங்களுக்கும் சீனாவே நிதி வழங்குகின்றது.
+இலங்கையில் நெருக்கடியான நிலைமையை ஏற்படுத்தவே சீனா இவ்வாறு செயற்பட்டு வருகின்றது
+சீனா விவகாரத்தில் கழுத்து நசுக்கப்பட்ட நிலைமையிலேயே நாம் உள்ளோம்
+ஆகவே எமது நாட்டை நெருக்கடிக்கு உள்ளாக்க வேண்டாம் என சீனாவிடம் நான் தலைகுனிந்து வேண்டுகின்றேன்.
+அத்துடன் பொன்சேகாவுக்கு அருகில் இருந்த பாதாள கோஷ்டி ஒருவரை கைது செய்ததை போன்று ஏனைய அமைச்சர்களின் பதாள கோஷ்டியினரை கைது செய்ய வேண்டும் என்றும் அவர் குறிப்பிட்டார்.
+திருமதி'''
+fixed_random_words=fixed_random_words.split(' ')
  
 parser = argparse.ArgumentParser()
  
@@ -37,14 +49,52 @@ def corrupt_homophones(data):
 
   return(output_data)
 
-def corrupt_gender(data):
+def corrupt_gender(data, corrupt_prob_percent=10):
   output_data=""
   for i in data.split(" "):
-    x=np.random.randint(2, size=1)[0]
+    x=np.random.randint(100, size=1)[0]
     y=i
-    if x==0:
+    if x <= corrupt_prob_percent:
       if re.findall("ள்$", i): y=re.sub("ள்$", "ன்", i)
       else: y=re.sub("ன்$", "ள்", i)
+    output_data+=y+" "
+  return(output_data)
+
+# def corrupt_tense(data):
+#   output_data=""
+#   for i in data.split(" "):
+#     x=np.random.randint(2, size=1)[0]
+#     y=i
+#     if x==0:
+#       if re.findall("ஂதாள்$", i): y=re.sub("ஂதாள்$", "ிருபஂபாள்", i)
+#       elif re.findall("ஂதான்$", i): y=re.sub("ஂதான்$", "ிருபஂபாள்", i)
+#       elif re.findall("ஂபாள்$", i): y=re.sub("ிருபஂபாள்$", "ஂதான்", i)
+#       elif re.findall("ஂபான்$", i): y=re.sub("ிருபஂபான்$", "ஂதான்", i)
+#     output_data+=y+" "
+#   return(output_data)
+
+def corrupt_pluarlity(data, corrupt_prob_percent=10):
+  output_data=""
+  for i in data.split(" "):
+    x=np.random.randint(100, size=1)[0]
+    y=i
+    if x<=corrupt_prob_percent:
+      if re.findall("ள்$", i): y=re.sub("ள்$", "ரஂ௧ளஂ", i)
+      else: y=re.sub("ன்$", "ரஂ௧ளஂ", i)
+    output_data+=y+" "
+  return(output_data)
+
+def corrupt_thinai(data, corrupt_prob_percent=10):
+  output_data=""
+  for i in data.split(" "):
+    x=np.random.randint(100, size=1)[0]+1
+    y=i
+    if(x <= corrupt_prob_percent):
+      if re.findall("ாள்$", i): y=re.sub("ாள்$", "து", i)
+      elif re.findall("ான்$", i): y=re.sub("ான்$", "து", i)
+      elif re.findall("ன்$", i): y=re.sub("ன்$", "து", i)
+      elif re.findall("ள்$", i): y=re.sub("ள்$", "து", i)
+      # elif re.findall("து$", i): y=re.sub("து$", "ள்", i)
     output_data+=y+" "
   return(output_data)
 
@@ -61,9 +111,38 @@ def remove_random_words(data, remove_prob_percent=5):
   for i in data.split(" "):
     x=np.random.randint(100, size=1)[0]+1
     if(x > remove_prob_percent):
-      output_data += i
+      output_data += i + " "
+  return(output_data)
+
+def repeat_words(data, repeat_prob_percentage=5):
+  output_data=""
+  for i in data.split(" "):
+    x=np.random.randint(100, size=1)[0]+1
+    if(x <= repeat_prob_percentage):
+      output_data += i + " "
     output_data+=i+" "
   return(output_data)
+
+def add_random_words(data, random_words=fixed_random_words, add_prob_percentage=5):
+  output_data=""
+  n = len(random_words)
+  if(n==0): return data
+  for i in data.split(" "):
+    x=np.random.randint(100, size=1)[0]+1
+    idx=np.random.randint(n, size=1)[0]
+    if(x <= add_prob_percentage):
+      output_data += random_words[idx] + " "
+    output_data+=i+" "
+  return(output_data)
+
+def merge_sentence(data, merge_prob_percent=10):
+  output_data = ""
+  for i in data:
+    if(i == '.'):
+      x=np.random.randint(100, size=1)[0]+1
+      if(x > merge_prob_percent):
+        output_data += " மற்றும்"
+    return output_data
 
 Tamil_Dataset = {
     'dataset_name': 'oscar',
@@ -119,10 +198,20 @@ for data in tqdm(train_dataset, total=dataset_len):
     f_org.write(d)
 
 
-  data=corrupt_gender(data)
-  data=corrupt_homophones(data)
-  data=remove_random_chars(data, 10)
+  # data=corrupt_gender(data)
+  # data=corrupt_homophones(data)
+  # data=remove_random_chars(data, 10)
 
+  # data_corrupted = list(data.split(". "))
+
+  data=remove_random_words(data, 10)
+  data=add_random_words(data, fixed_random_words, 30)
+  data=repeat_words(data, 30)
+  data=merge_sentence(data, 30)
+  data=corrupt_thinai(data, 30)
+  data=corrupt_gender(data, 30)
+  data=corrupt_homophones(data)
+  data=remove_random_chars(data)
   data_corrupted = list(data.split(". "))
 
   for d in data_corrupted:
@@ -154,8 +243,12 @@ for data in tqdm(test_dataset, total=dataset_len):
     f_org.write(d)
 
 
-  data=remove_random_words(data)
-  data=corrupt_gender(data)
+  data=remove_random_words(data, 10)
+  data=add_random_words(data, fixed_random_words, 30)
+  data=repeat_words(data, 30)
+  data=merge_sentence(data, 30)
+  data=corrupt_thinai(data, 30)
+  data=corrupt_gender(data, 30)
   data=corrupt_homophones(data)
   data=remove_random_chars(data)
   data_corrupted = list(data.split(". "))
@@ -175,4 +268,4 @@ f_cor.close()
 இருந்தாள்
 மா
 ாம
-''''''
+'''
--- temp.py
+++ temp.py
@@ -1,6 +1,8 @@
 from distutils.file_util import write_file
 from datasets import load_dataset
 from tqdm import tqdm
+import numpy as np
+import re
 
 Tamil_Dataset = {
     'dataset_name': 'oscar',
@@ -21,7 +23,26 @@ for sentence in train_dataset:
     for sent in doc.sentences:
         for word in sent.words:
             if(word.pos=='VERB'):
-                print(f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}')
+                # print(f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}')
+                i = word.text
+                y= i
+                if re.findall("ஂதாள்$", i): y=re.sub("ஂதாள்$", "ிருபஂபாள்", i)
+                elif re.findall("ஂதான்$", i): y=re.sub("ஂதான்$", "ிருபஂபாள்", i)
+                elif re.findall("ஂபாள்$", i): y=re.sub("ிருபஂபாள்$", "ஂதான்", i)
+                elif re.findall("ஂபான்$", i): y=re.sub("ிருபஂபான்$", "ஂதான்", i)
+                if re.findall("ாள்$", i): y=re.sub("ாள்$", "து", i)
+                if(i != y):
+                    print('i', i, ' y', y)
+                elif re.findall("ான்$", i): y=re.sub("ான்$", "து", i)
+                elif re.findall("ன்$", i): y=re.sub("ன்$", "து", i)
+                elif re.findall("ள்$", i): y=re.sub("ள்$", "து", i)
+                if(i != y):
+                    print('i', i, ' y', y)
+                if re.findall("ள்$", i): y=re.sub("ள்$", "ரஂ௧ளஂ", i)
+                else: y=re.sub("ன்$", "ரஂ௧ளஂ", i)
+                if(i != y):
+                    print('i', i, ' y', y)
+                
     # doc.sentences[0].print_dependencies()
     # print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
     # write_file.write(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
